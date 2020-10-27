bbe709323fb34b3316927bbe1cc83e71454a2c95
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-10-27 23:39:00 +0530

    Created product order service

---
+++ inventory-product-order-service/src/main/java/com/cg/inventoryproductorderservice/InventoryProductOrderServiceApplication.java
@@ -0,0 +1,20 @@
+/**
+ * @author Gagandeep Singh
+ * @email singh.gagandeep3911@gmail.com
+ * @create date 2020-10-27 23:38:13
+ * @modify date 2020-10-27 23:38:13
+ * @desc Product Order Service - Entrypoint
+ */
+package com.cg.inventoryproductorderservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryProductOrderServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryProductOrderServiceApplication.class, args);
+	}
+
+}
---
+++ inventory-product-order-service/src/test/java/com/cg/inventoryproductorderservice/InventoryProductOrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cg.inventoryproductorderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryProductOrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
