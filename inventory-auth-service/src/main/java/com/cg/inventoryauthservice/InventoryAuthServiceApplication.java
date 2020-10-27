c28fa43fd3073b0e0fd6e0410762704f59413120
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-10-27 23:31:36 +0530

    Added eureka server support

--- inventory-auth-service/src/main/java/com/cg/inventoryauthservice/InventoryAuthServiceApplication.java
+++ inventory-auth-service/src/main/java/com/cg/inventoryauthservice/InventoryAuthServiceApplication.java
@@ -1,9 +1,18 @@
+/**
+ * @author Gagandeep Singh
+ * @email singh.gagandeep3911@gmail.com
+ * @create date 2020-10-27 23:29:16
+ * @modify date 2020-10-27 23:29:16
+ * @desc Starting point
+ */
 package com.cg.inventoryauthservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class InventoryAuthServiceApplication {
 
 	public static void main(String[] args) {
