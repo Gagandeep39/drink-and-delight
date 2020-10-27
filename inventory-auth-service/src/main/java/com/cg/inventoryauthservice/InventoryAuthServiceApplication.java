9b8d3b045543cc789ed3b81eaa6e98a601e48282
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-10-27 23:28:59 +0530

    Created auth Service

---
+++ inventory-auth-service/src/main/java/com/cg/inventoryauthservice/InventoryAuthServiceApplication.java
@@ -0,0 +1,13 @@
+package com.cg.inventoryauthservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryAuthServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryAuthServiceApplication.class, args);
+	}
+
+}
---
+++ inventory-auth-service/src/test/java/com/cg/inventoryauthservice/InventoryAuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cg.inventoryauthservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryAuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
