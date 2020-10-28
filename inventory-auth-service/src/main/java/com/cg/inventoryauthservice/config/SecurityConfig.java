2dee9c09f4c3b69b2e1a45bddf84cbc11ecf5c52
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-10-28 11:41:27 +0530

    Added spring security
    Password now encoded with bcrypt
    Enabled routing in all routes for now

---
+++ inventory-auth-service/src/main/java/com/cg/inventoryauthservice/config/SecurityConfig.java
@@ -0,0 +1,24 @@
+package com.cg.inventoryauthservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http.authorizeRequests().antMatchers("/**").permitAll();
+    http.csrf().disable();
+  }
+
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
+
+}
