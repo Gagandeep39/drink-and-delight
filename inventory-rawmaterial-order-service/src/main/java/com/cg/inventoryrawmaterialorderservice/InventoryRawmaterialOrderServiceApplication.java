c47a0cb081400c7666b0989bcb7eda5925dbe51b
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-10-28 08:42:54 +0530

    Added profiles Added eureka support

--- inventory-rawmaterial-order-service/src/main/java/com/cg/inventoryrawmaterialorderservice/InventoryRawmaterialOrderServiceApplication.java
+++ inventory-rawmaterial-order-service/src/main/java/com/cg/inventoryrawmaterialorderservice/InventoryRawmaterialOrderServiceApplication.java
@@ -2,8 +2,10 @@ package com.cg.inventoryrawmaterialorderservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class InventoryRawmaterialOrderServiceApplication {
 
 	public static void main(String[] args) {
