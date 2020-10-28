a995a755070c27bae38ede70fe2cd18ebc4baf98
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-10-28 08:43:53 +0530

    Created stock management service

---
+++ inventory-stock-management-service/src/main/java/com/cg/inventorystockmanagementservice/InventoryStockManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.cg.inventorystockmanagementservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryStockManagementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryStockManagementServiceApplication.class, args);
+	}
+
+}
