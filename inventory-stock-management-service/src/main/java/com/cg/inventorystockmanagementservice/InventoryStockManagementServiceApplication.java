bf08a8a85199b4b8e739e4ecebb40debc04d9696
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-10-28 08:49:10 +0530

    Added eureka client support

--- inventory-stock-management-service/src/main/java/com/cg/inventorystockmanagementservice/InventoryStockManagementServiceApplication.java
+++ inventory-stock-management-service/src/main/java/com/cg/inventorystockmanagementservice/InventoryStockManagementServiceApplication.java
@@ -2,8 +2,10 @@ package com.cg.inventorystockmanagementservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class InventoryStockManagementServiceApplication {
 
 	public static void main(String[] args) {
