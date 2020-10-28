d86ff018af0e062056f98710911053da85551aa2
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-10-28 08:37:13 +0530

    Created raw material service

---
+++ inventory-rawmaterial-order-service/src/main/java/com/cg/inventoryrawmaterialorderservice/InventoryRawmaterialOrderServiceApplication.java
@@ -0,0 +1,13 @@
+package com.cg.inventoryrawmaterialorderservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryRawmaterialOrderServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryRawmaterialOrderServiceApplication.class, args);
+	}
+
+}
---
+++ inventory-rawmaterial-order-service/src/test/java/com/cg/inventoryrawmaterialorderservice/InventoryRawmaterialOrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cg.inventoryrawmaterialorderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryRawmaterialOrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
