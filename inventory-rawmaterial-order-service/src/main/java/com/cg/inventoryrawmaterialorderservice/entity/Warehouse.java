bc4843b340e8b9e95ea362a24735a2c6f2003262
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-10-28 09:04:22 +0530

    Created enums and entities

---
+++ inventory-rawmaterial-order-service/src/main/java/com/cg/inventoryrawmaterialorderservice/entity/Warehouse.java
@@ -0,0 +1,36 @@
+/**
+ * @author Gagandeep Singh
+ * @email singh.gagandeep3911@gmail.com
+ * @create date 2020-09-19 19:07:14
+ * @modify date 2020-09-19 19:07:14
+ * @desc Warehouse Entity
+ */
+package com.cg.inventoryrawmaterialorderservice.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Warehouse {
+
+  @Id
+  @SequenceGenerator(name = "warehouse_id_sequence", initialValue = 100000, allocationSize = 1)
+  @GeneratedValue(generator = "warehouse_id_sequence", strategy = GenerationType.SEQUENCE)
+  private Long warehouseId;
+  @Column(length = 20)
+  private String name;
+  @Column(length = 50)
+  private String description;
+
+}
---
+++ inventory-rawmaterial-order-service/src/main/java/com/cg/inventoryrawmaterialorderservice/enums/MeasurementUnit.java
@@ -0,0 +1,5 @@
+package com.cg.inventoryrawmaterialorderservice.enums;
+
+public enum MeasurementUnit {
+  Kilogram, Litre
+}
