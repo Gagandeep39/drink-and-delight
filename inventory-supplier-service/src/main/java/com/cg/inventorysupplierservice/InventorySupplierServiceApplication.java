e8d955ca7cff95b0073686692c53e900cfa24f2f
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-10-28 08:50:53 +0530

    Created supplier service

---
+++ inventory-supplier-service/src/main/java/com/cg/inventorysupplierservice/InventorySupplierServiceApplication.java
@@ -0,0 +1,13 @@
+package com.cg.inventorysupplierservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventorySupplierServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventorySupplierServiceApplication.class, args);
+	}
+
+}
---
+++ inventory-supplier-service/src/test/java/com/cg/inventorysupplierservice/InventorySupplierServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cg.inventorysupplierservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventorySupplierServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
