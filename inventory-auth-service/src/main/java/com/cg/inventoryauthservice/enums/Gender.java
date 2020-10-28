485fb5e003de0de9b9c032ddb21a3db8118b57e7
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-10-28 15:51:07 +0530

    Created Enums

---
+++ inventory-auth-service/src/main/java/com/cg/inventoryauthservice/enums/Gender.java
@@ -0,0 +1,12 @@
+/**
+ * @author Gagandeep Singh
+ * @email singh.gagandeep3911@gmail.com
+ * @create date 2020-09-19 18:33:16
+ * @modify date 2020-09-19 18:33:16
+ * @desc Gender Enum
+ */
+package com.cg.inventoryauthservice.enums;
+
+public enum Gender {
+  Male, Female
+}
