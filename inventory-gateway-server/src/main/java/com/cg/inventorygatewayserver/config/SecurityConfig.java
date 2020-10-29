59da4eecc8d91aaf7c3e9e26a87a67b01c229e09
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-10-29 18:09:05 +0530

    Added security config with all paths

---
+++ inventory-gateway-server/src/main/java/com/cg/inventorygatewayserver/config/SecurityConfig.java
@@ -0,0 +1,74 @@
+/**
+ * @author Gagandeep Singh
+ * @email singh.gagandeep3911@gmail.com
+ * @create date 2020-10-29 16:50:12
+ * @modify date 2020-10-29 16:50:12
+ * @desc Security COnfiguration
+ */
+package com.cg.inventorygatewayserver.config;
+
+import com.cg.inventorygatewayserver.security.CustomAuthenticationEntryPoint;
+import com.cg.inventorygatewayserver.security.JwtAuthenticationFilter;
+import com.cg.inventorygatewayserver.service.implementation.JwtUserDetailsServiceImpl;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import lombok.AllArgsConstructor;
+
+@EnableWebSecurity
+@AllArgsConstructor
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+  private JwtAuthenticationFilter jwtAuthenticationFilter;
+  private JwtUserDetailsServiceImpl userDetailsService;
+
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http.csrf().disable()
+      .authorizeRequests().antMatchers("/inventory-auth-service/**", "/actuator/**", "/**/h2", "/**/swagger*/**", "/**/v2/api-docs").permitAll().antMatchers("/h2").permitAll()
+      .anyRequest().authenticated()
+      .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+      .and().exceptionHandling().authenticationEntryPoint(authenticationEntryPoint());
+    http.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+  }
+
+  /**
+   * Password Encodeer DI
+   */
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
+
+  @Override
+  protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+  }
+
+  /**
+   * In @Bean(BeanIds.AUTHENTICATION_MANAGER) NeanIds part is optional
+   */
+  @Bean
+  @Override
+  public AuthenticationManager authenticationManagerBean() throws Exception {
+    return super.authenticationManagerBean();
+  }
+
+  /**
+   * DI for CustomAuthentiaction EntryPoint
+   */
+  public AuthenticationEntryPoint authenticationEntryPoint() {
+    return new CustomAuthenticationEntryPoint();
+  }
+
+}
