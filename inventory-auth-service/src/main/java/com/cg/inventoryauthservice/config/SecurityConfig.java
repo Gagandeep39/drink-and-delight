e96f6df08f4dd6973f5722b3d05871df91e16b16
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-10-29 18:14:33 +0530

    Fixed unaccessible paths in Auth service

--- inventory-auth-service/src/main/java/com/cg/inventoryauthservice/config/SecurityConfig.java
+++ inventory-auth-service/src/main/java/com/cg/inventoryauthservice/config/SecurityConfig.java
@@ -28,7 +28,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
   @Override
   protected void configure(HttpSecurity http) throws Exception {
     http.csrf().disable()
-      .authorizeRequests().antMatchers("/auth/**").permitAll().antMatchers("/h2").permitAll()
+      .authorizeRequests().antMatchers("/auth/**", "/h2", "/swagger*/**", "/v2/api-docs").permitAll().antMatchers().permitAll()
       .anyRequest().authenticated()
       .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
       .and().exceptionHandling().authenticationEntryPoint(authenticationEntryPoint());
