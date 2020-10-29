e3846dfedf2d26e8ebbbf7b31d8c9864b3f4f42d
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-10-29 16:18:43 +0530

    Moved Eureka client toggle to separate file

---
+++ inventory-auth-service/src/main/java/com/cg/inventoryauthservice/config/DiscoveryClientConfig.java
@@ -0,0 +1,17 @@
+/**
+ * @author Gagandeep Singh
+ * @email singh.gagandeep3911@gmail.com
+ * @create date 2020-10-29 16:18:08
+ * @modify date 2020-10-29 16:18:08
+ * @desc Discovery client confi
+ */
+package com.cg.inventoryauthservice.config;
+
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Profile;
+
+@Profile("!test")
+@EnableDiscoveryClient
+public class DiscoveryClientConfig {
+  
+}
