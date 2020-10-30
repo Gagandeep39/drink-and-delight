e7d90e77b9e9436cd35d4f64ae62a007562947b4
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-10-30 17:53:59 +0530

    Removed unused imports

--- inventory-rawmaterial-order-service/src/main/java/com/cg/inventoryrawmaterialorderservice/repository/RawMaterialOrderRepository.java
+++ inventory-rawmaterial-order-service/src/main/java/com/cg/inventoryrawmaterialorderservice/repository/RawMaterialOrderRepository.java
@@ -1,6 +1,11 @@
+/**
+ * @author Gagandeep Singh
+ * @email singh.gagandeep3911@gmail.com
+ * @create date 2020-10-30 17:53:13
+ * @modify date 2020-10-30 17:53:13
+ * @desc RM order repository
+ */
 package com.cg.inventoryrawmaterialorderservice.repository;
-
-import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -8,8 +13,6 @@ import org.springframework.stereotype.Repository;
 import com.cg.inventoryrawmaterialorderservice.entity.RawMaterialOrder;
 
 @Repository
-public interface RawMaterialOrderRepository extends JpaRepository<RawMaterialOrder, Long>{
+public interface RawMaterialOrderRepository extends JpaRepository<RawMaterialOrder, Long> {
 
-
-	
 }
