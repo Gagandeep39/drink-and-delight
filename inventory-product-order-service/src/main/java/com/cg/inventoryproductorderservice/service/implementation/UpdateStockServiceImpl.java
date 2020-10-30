e920f4f6cfb7a8475844d1cb1936453983fcbfc6
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-10-31 03:11:32 +0530

    Created Update stock service

---
+++ inventory-product-order-service/src/main/java/com/cg/inventoryproductorderservice/service/implementation/UpdateStockServiceImpl.java
@@ -0,0 +1,35 @@
+/**
+ * @author Gagandeep Singh
+ * @email singh.gagandeep3911@gmail.com
+ * @create date 2020-10-31 02:31:27
+ * @modify date 2020-10-31 02:31:27
+ * @desc Update stock Implementation
+ */
+package com.cg.inventoryproductorderservice.service.implementation;
+
+import com.cg.inventoryproductorderservice.dto.UpdateStockRequest;
+import com.cg.inventoryproductorderservice.entity.Product;
+import com.cg.inventoryproductorderservice.service.UpdateStockService;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import lombok.AllArgsConstructor;
+
+@Service
+@AllArgsConstructor
+public class UpdateStockServiceImpl implements UpdateStockService {
+
+  @Qualifier("loadbalanced")
+  private final RestTemplate restTemplate;
+
+  @Async
+  @Override
+  public Product updateProductStock(Long productId, Double quantity) {
+    restTemplate.put("http://inventory-stock-management-service/productStock", new UpdateStockRequest(productId, quantity));
+    return null;
+  }
+
+}
