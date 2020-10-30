20afa398476b4c6fb2cedd7a746ece425b90290c
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-10-30 15:59:05 +0530

    Created Exception handler

--- inventory-rawmaterial-order-service/src/main/java/com/cg/inventoryrawmaterialorderservice/exception/CustomExceptionHandler.java
+++ inventory-rawmaterial-order-service/src/main/java/com/cg/inventoryrawmaterialorderservice/exception/CustomExceptionHandler.java
@@ -56,6 +56,19 @@ public class CustomExceptionHandler {
                     .build())).build());
     }
 
+    @ExceptionHandler(InvalidOrderUpdateStatusException.class)
+    public ResponseEntity<ErrorResponse> handleInvalidStatus(InvalidOrderUpdateStatusException ex) {
+        return ResponseEntity
+            .status(HttpStatus.BAD_REQUEST)
+            .body(ErrorResponse.builder()
+                .timeStamp(System.currentTimeMillis())
+                .status(HttpStatus.BAD_REQUEST.value()).message("FieldException")
+                .errors(Collections.singletonList(FieldErrorResponse.builder()
+                    .field(ex.getErrorName())
+                    .message(ex.getErrorDescription())
+                    .build())).build());
+    }
+
     @ExceptionHandler
     public ResponseEntity<ErrorResponse> catchAllException(Exception exception) {
         return ResponseEntity
