8e195731c0dcee9ff84cbd44544742c8bf886306
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-10-31 01:14:28 +0530

    Updated Entities

--- inventory-stock-management-service/src/main/java/com/cg/inventorystockmanagementservice/entity/Product.java
+++ inventory-stock-management-service/src/main/java/com/cg/inventorystockmanagementservice/entity/Product.java
@@ -1,12 +1,11 @@
-package com.cg.inventorystockmanagementservice.entity;
-
 /**
- * @author Shubham Sangale
- * @email sssvsangale@gmail.com
+ * @author Gagandeep Singh
+ * @email singh.gagandeep3911@gmail.com
  * @create date 2020-09-19 19:20:33
  * @modify date 2020-09-19 19:20:33
  * @desc Product properties
  */
+package com.cg.inventorystockmanagementservice.entity;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -26,101 +25,24 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 public class Product {
 
-	@Id
-	@SequenceGenerator(name = "product_id_sequence", initialValue = 100000, allocationSize = 1)
-	@GeneratedValue(generator = "product_id_sequence", strategy = GenerationType.SEQUENCE)
-	private Long productId;
-	@Column(length = 20)
-	private String materialName;
-	@Column(length = 100)
-	private String description;
-	private Double quantityAvailable;
-	@Enumerated(EnumType.STRING)
-	private MeasurementUnit quantityUnit;
-	@ManyToOne
-	@JoinColumn(name = "warehouseId", referencedColumnName = "warehouseId", foreignKey = @ForeignKey(name = "FK_prod_warehouse_ID"))
-	private Warehouse warehouse;
-
-	public Long getProductId() {
-		return productId;
-	}
-
-	public void setProductId(Long productId) {
-		this.productId = productId;
-	}
-
-	public String getMaterialName() {
-		return materialName;
-	}
-
-	public void setMaterialName(String materialName) {
-		this.materialName = materialName;
-	}
-
-	public String getDescription() {
-		return description;
-	}
-
-	public void setDescription(String description) {
-		this.description = description;
-	}
-
-	public Double getQuantityAvailable() {
-		return quantityAvailable;
-	}
-
-	public void setQuantityAvailable(Double quantityAvailable) {
-		this.quantityAvailable = quantityAvailable;
-	}
-
-	public Product(String materialName, String description, Double quantityAvailable, MeasurementUnit quantityUnit,
-			Warehouse warehouse) {
-		super();
-		this.materialName = materialName;
-		this.description = description;
-		this.quantityAvailable = quantityAvailable;
-		this.quantityUnit = quantityUnit;
-		this.warehouse = warehouse;
-	}
-
-	public Product(Long productId, String materialName, String description, Double quantityAvailable,
-			MeasurementUnit quantityUnit, Warehouse warehouse) {
-		super();
-		this.productId = productId;
-		this.materialName = materialName;
-		this.description = description;
-		this.quantityAvailable = quantityAvailable;
-		this.quantityUnit = quantityUnit;
-		this.warehouse = warehouse;
-	}
-
-	public Product() {
-		super();
-	}
-
-	@Override
-	public String toString() {
-		return "Product [productId=" + productId + ", materialName=" + materialName + ", description=" + description
-				+ ", quantityAvailable=" + quantityAvailable + ", quantityUnit=" + quantityUnit + ", warehouse="
-				+ warehouse + "]";
-	}
-
-	public MeasurementUnit getQuantityUnit() {
-		return quantityUnit;
-	}
-
-	public void setQuantityUnit(MeasurementUnit quantityUnit) {
-		this.quantityUnit = quantityUnit;
-	}
-
-	public Warehouse getWarehouse() {
-		return warehouse;
-	}
-
-	public void setWarehouse(Warehouse warehouse) {
-		this.warehouse = warehouse;
-	}
+  @Id
+  @SequenceGenerator(name = "product_id_sequence", initialValue = 100000, allocationSize = 1)
+  @GeneratedValue(generator = "product_id_sequence", strategy = GenerationType.SEQUENCE)
+  private Long productId;
+  @Column(length = 20)
+  private String materialName;
+  @Column(length = 100)
+  private String description;
+  private Double quantityAvailable;
+  @Enumerated(EnumType.STRING)
+  private MeasurementUnit quantityUnit;
+  @ManyToOne
+  @JoinColumn(name = "warehouseId", referencedColumnName = "warehouseId", foreignKey = @ForeignKey(name = "FK_prod_warehouse_ID"))
+  private Warehouse warehouse;
 }
--- inventory-stock-management-service/src/main/java/com/cg/inventorystockmanagementservice/entity/Warehouse.java
+++ inventory-stock-management-service/src/main/java/com/cg/inventorystockmanagementservice/entity/Warehouse.java
@@ -1,7 +1,6 @@
-
 /**
- * @author Shubham Sangale
- * @email sssvsangale@gmail.com
+ * @author Gagandeep Singh
+ * @email singh.gagandeep3911@gmail.com
  * @create date 2020-09-19 19:07:14
  * @modify date 2020-09-19 19:07:14
  * @desc Warehouse Entity
@@ -19,62 +18,19 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 public class Warehouse {
 
-	@Id
-	@SequenceGenerator(name = "warehouse_id_sequence", initialValue = 100000, allocationSize = 1)
-	@GeneratedValue(generator = "warehouse_id_sequence", strategy = GenerationType.SEQUENCE)
-	private Long warehouseId;
-	@Column(length = 20)
-	private String name;
-	@Column(length = 50)
-	private String description;
-
-	public Warehouse() {
-		super();
-	}
-
-	public Warehouse(Long warehouseId, String name, String description) {
-		super();
-		this.warehouseId = warehouseId;
-		this.name = name;
-		this.description = description;
-	}
-
-	public Warehouse(String name, String description) {
-		super();
-		this.name = name;
-		this.description = description;
-	}
-
-	public Long getWarehouseId() {
-		return warehouseId;
-	}
-
-	public void setWarehouseId(Long warehouseId) {
-		this.warehouseId = warehouseId;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getDescription() {
-		return description;
-	}
-
-	public void setDescription(String description) {
-		this.description = description;
-	}
-
-	@Override
-	public String toString() {
-		return "Warehouse [warehouseId=" + warehouseId + ", name=" + name + ", description=" + description + "]";
-	}
+  @Id
+  @SequenceGenerator(name = "warehouse_id_sequence", initialValue = 100000, allocationSize = 1)
+  @GeneratedValue(generator = "warehouse_id_sequence", strategy = GenerationType.SEQUENCE)
+  private Long warehouseId;
+  @Column(length = 20)
+  private String name;
+  @Column(length = 50)
+  private String description;
 
 }
