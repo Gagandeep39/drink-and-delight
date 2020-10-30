13dfc73128d91f378908963f59b5f87348b96704
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-10-31 03:12:12 +0530

    Created Load balancer in rm service

---
+++ inventory-rawmaterial-order-service/src/main/java/com/cg/inventoryrawmaterialorderservice/config/LoadBalancerConfig.java
@@ -0,0 +1,35 @@
+/**
+ * @author Gagandeep Singh
+ * @email singh.gagandeep3911@gmail.com
+ * @create date 2020-10-31 03:03:31
+ * @modify date 2020-10-31 03:03:31
+ * @desc Performs Load balancing
+ */
+package com.cg.inventoryrawmaterialorderservice.config;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class LoadBalancerConfig {
+
+  public static final String LOADBALANCED = "loadbalanced";
+  public static final String STANDARD = "standard";
+
+  @LoadBalanced
+  @Bean
+  @Qualifier(LOADBALANCED)
+  public RestTemplate loadBalancedRestTemplate() {
+    return new RestTemplate();
+  }
+
+  // @Bean
+  // @Qualifier(STANDARD)
+  // public RestTemplate restTemplate() {
+  //   return new RestTemplate();
+  // }
+
+}
