02a8fc6ef61fc0e0290a80fb651cfa9fb86d1a3b
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-10-31 00:12:30 +0530

    Updated mapper

--- inventory-product-order-service/src/main/java/com/cg/inventoryproductorderservice/helper/ProductOrderMapper.java
+++ inventory-product-order-service/src/main/java/com/cg/inventoryproductorderservice/helper/ProductOrderMapper.java
@@ -6,36 +6,42 @@ import com.cg.inventoryproductorderservice.dto.ProductOrderRequest;
 import com.cg.inventoryproductorderservice.dto.ProductOrderResponse;
 import com.cg.inventoryproductorderservice.entity.Product;
 import com.cg.inventoryproductorderservice.entity.ProductOrder;
-import com.cg.inventoryproductorderservice.entity.Supplier;
-import com.cg.inventoryproductorderservice.enums.OrderStatus;
 import com.cg.inventoryproductorderservice.enums.QualityCheck;
 
 public class ProductOrderMapper {
 
 	
 	public static ProductOrderResponse entityToDto(ProductOrder productOrder) {
-		return ProductOrderResponse.builder().productName(productOrder.getProduct().getMaterialName())
-				.productOrderId(productOrder.getProductOrderId()).quantity(productOrder.getQuantity())
-				.qualityCheck(productOrder.getQualityCheck()).orderStatus(productOrder.getOrderStatus())
-				.deliveryDate(productOrder.getDilveryDate()).expiryDate(productOrder.getExpiryDate())
-				.orderedOn(productOrder.getOrderedOn()).supplierName(productOrder.getSupplier().getName())
-				.supplierId(productOrder.getSupplier().getSupplierId())
-				.warehouse(productOrder.getProduct().getWarehouse())
-				.measurementUnit(productOrder.getProduct().getQuantityUnit())
-				.description(productOrder.getProduct().getDescription()).build();
+		return ProductOrderResponse.builder()
+			.productOrderId(productOrder.getProductOrderId())
+			.quantity(productOrder.getQuantity())
+			.pricePerUnit(productOrder.getPricePerUnit())
+			.qualityCheck(productOrder.getQualityCheck())
+			.orderStatus(productOrder.getOrderStatus())
+			.deliveryDate(productOrder.getDeliveryDate())
+			.manufactureDate(productOrder.getManufactureDate())
+			.expiryDate(productOrder.getExpiryDate())
+			.orderedOn(productOrder.getOrderedOn())
+			.distributorName(productOrder.getDistributor().getName())
+			.distributorId(productOrder.getDistributor().getDistributorId())
+			.productName(productOrder.getProduct().getMaterialName())
+			.description(productOrder.getProduct().getDescription())
+			.measurementUnit(productOrder.getProduct().getQuantityUnit())
+			.warehouse(productOrder.getProduct().getWarehouse())
+			.build();
 	}
 	
 	
 	public static ProductOrder DtoToEntity(ProductOrderRequest request) {
 		ProductOrder order = new ProductOrder();
-		Supplier supplier = new Supplier();
-		supplier.setSupplierId(request.getSupplierId());
+		// Supplier supplier = new Supplier();
+		// supplier.setSupplierId(request.getSupplierId());
 		Product product = new Product();
 		product.setProductId(request.getProductId());
-		order.setDilveryDate(request.getDeliveryDate());
-		order.setExpiryDate(request.getExpiryDate());
-		order.setOrderStatus(OrderStatus.Processing);
-		order.setSupplier(supplier);
+		// order.setDilveryDate(request.getDeliveryDate());
+		// order.setExpiryDate(request.getExpiryDate());
+		// order.setOrderStatus(OrderStatus.Processing);
+		// order.setSupplier(supplier);
 		order.setPricePerUnit(request.getPricePerUnit());
 		order.setQuantity(request.getQuantity());
 		order.setOrderedOn(LocalDate.now());
