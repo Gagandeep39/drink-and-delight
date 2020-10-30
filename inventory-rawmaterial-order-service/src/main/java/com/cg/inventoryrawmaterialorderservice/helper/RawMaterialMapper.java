628913cc54a76bcb4589440d9953881eeb108ee5
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-10-30 16:49:51 +0530

    Created mapper

--- inventory-rawmaterial-order-service/src/main/java/com/cg/inventoryrawmaterialorderservice/helper/RawMaterialMapper.java
+++ inventory-rawmaterial-order-service/src/main/java/com/cg/inventoryrawmaterialorderservice/helper/RawMaterialMapper.java
@@ -7,8 +7,15 @@
  */
 package com.cg.inventoryrawmaterialorderservice.helper;
 
+import java.time.LocalDate;
+
+import com.cg.inventoryrawmaterialorderservice.dto.RawMaterialOrderRequest;
 import com.cg.inventoryrawmaterialorderservice.dto.RawMaterialOrderResponse;
+import com.cg.inventoryrawmaterialorderservice.entity.RawMaterial;
 import com.cg.inventoryrawmaterialorderservice.entity.RawMaterialOrder;
+import com.cg.inventoryrawmaterialorderservice.entity.Supplier;
+import com.cg.inventoryrawmaterialorderservice.enums.OrderStatus;
+import com.cg.inventoryrawmaterialorderservice.enums.QualityCheck;
 
 public class RawMaterialMapper {
 
@@ -28,5 +35,24 @@ public class RawMaterialMapper {
       .measurementUnit(rawMaterialOrder.getRawMaterial().getQuantityUnit())
       .description(rawMaterialOrder.getRawMaterial().getDescription())
       .build();
+  }
+
+  
+  public static RawMaterialOrder DtoToEntity(RawMaterialOrderRequest request) {
+    RawMaterialOrder order = new RawMaterialOrder();
+    Supplier supplier = new Supplier();
+    supplier.setSupplierId(request.getSupplierId());
+    RawMaterial rawMaterial = new RawMaterial();
+    rawMaterial.setRawMaterialId(request.getRawMaterialId());
+    order.setDeliveryDate(request.getDeliveryDate());
+    order.setExpiryDate(request.getExpiryDate());
+    order.setOrderStatus(OrderStatus.Processing);
+    order.setSupplier(supplier);
+    order.setPricePerUnit(request.getPricePerUnit());
+    order.setQuantity(request.getQuantity());
+    order.setOrderedOn(LocalDate.now());
+    order.setQualityCheck(QualityCheck.valueOf(request.getQualityCheck()));
+    order.setRawMaterial(rawMaterial);
+    return order;
   }
 }
