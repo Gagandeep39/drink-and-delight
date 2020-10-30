5dd1ffe452a588a6ebe8bef9dea4fe7f40d249f4
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-10-30 20:39:54 +0530

    Removed unused import

--- inventory-auth-service/src/main/java/com/cg/inventoryauthservice/service/implementation/AuthServiceImpl.java
+++ inventory-auth-service/src/main/java/com/cg/inventoryauthservice/service/implementation/AuthServiceImpl.java
@@ -35,7 +35,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import lombok.AllArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 
 @Service
 @AllArgsConstructor
