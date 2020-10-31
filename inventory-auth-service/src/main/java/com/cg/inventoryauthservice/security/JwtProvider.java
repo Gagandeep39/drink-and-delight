24eaaac62bb09389741989e52d79b999404e1f23
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-11-01 01:10:20 +0530

    Updated jwt providers

--- inventory-auth-service/src/main/java/com/cg/inventoryauthservice/security/JwtProvider.java
+++ inventory-auth-service/src/main/java/com/cg/inventoryauthservice/security/JwtProvider.java
@@ -7,14 +7,14 @@
  */
 package com.cg.inventoryauthservice.security;
 
-import java.security.Key;
 import java.time.Instant;
 import java.util.Date;
 
-import javax.crypto.spec.SecretKeySpec;
-import javax.xml.bind.DatatypeConverter;
+import javax.crypto.SecretKey;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
@@ -22,6 +22,7 @@ import org.springframework.stereotype.Component;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
 
 @Component
 public class JwtProvider {
@@ -29,17 +30,13 @@ public class JwtProvider {
   @Value("${jwt.expiration}")
   private Long jwtExpirationInMillis;
 
-  /**
-   * Temporariry Hardcoded string
-   */
-  // @Value("${jwt.key}")
-  private String secretKey = "oeRaYY7Wo24sDqKSX3IM9ASGmdGPmkTd9jo1QTy4b7P9Ze5_9hKolVX8xNrQDcNRfVEdTZNOuOyqEGhXEbdJI-ZQ19k_o9MI0y3eZN2lp9jow55FfXMiINEdt1XR85VipRLSOkT6kSpzs2x-jbLDiz9iFVzkd81YKxMgPA7VfZeQUm4n-mOmnWMaVX30zGFU4L3oPBctYKkl4dYfqYWqRNfrgPJVi5DGFjywgxx0ASEiJHtV72paI3fDR2XwlSkyhhmY-ICjCRmsJN4fX1pdoL8a18-aQrvyu4j0Os6dVPYIoPvvY0SAZtWYKHfM15g7A3HD4cVREf9cUsprCRK93w";
+  @Value("${jwt.secret}")
+  private String secret;
+
+  @Autowired
+  private SecretKey secretKey;
 
   SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.HS256;
-
-  //We will sign our JWT with our ApiKey secret
-  byte[] apiKeySecretBytes = DatatypeConverter.parseBase64Binary(secretKey);
-  Key signingKey = new SecretKeySpec(apiKeySecretBytes, signatureAlgorithm.getJcaName());
 
   /**
    * Generate a token for signed in user
@@ -48,7 +45,7 @@ public class JwtProvider {
     UserDetails principal =  (UserDetails) authentication.getPrincipal();
     return Jwts.builder()
       .setSubject(principal.getUsername())
-      .signWith(signingKey)
+      .signWith(secretKey, signatureAlgorithm)
       .setExpiration(Date.from(Instant.now().plusMillis(jwtExpirationInMillis)))
       .compact();
   }
@@ -57,7 +54,7 @@ public class JwtProvider {
    * Check token validity
    */
   public boolean validateToken(String jwt) {
-    Jwts.parserBuilder().setSigningKey(signingKey).build().parseClaimsJws(jwt);
+    Jwts.parserBuilder().setSigningKey(secretKey).build().parseClaimsJws(jwt);
     return true;
   }
 
@@ -65,12 +62,8 @@ public class JwtProvider {
    * Feth username from JWT
    */
   public String getUsernameFromJwt(String token) {
-    Claims claims = Jwts.parserBuilder().setSigningKey(signingKey).build().parseClaimsJws(token).getBody();
+    Claims claims = Jwts.parserBuilder().setSigningKey(secretKey).build().parseClaimsJws(token).getBody();
     return claims.getSubject();
-  }
-
-  public long getExpirationInMillis() {
-    return jwtExpirationInMillis;
   }
 
   /**
@@ -82,8 +75,13 @@ public class JwtProvider {
     return Jwts.builder()
       .setSubject(username)
       .setIssuedAt(Date.from(Instant.now()))
-      .signWith(signingKey)
+      .signWith(secretKey, signatureAlgorithm)
       .setExpiration(Date.from(Instant.now().plusMillis(jwtExpirationInMillis))).compact();
+  }
+
+  @Bean
+  public SecretKey getSecretKey () {
+    return Keys.hmacShaKeyFor(secret.getBytes());
   }
 
 }
