c2089229fe8603e3b6ca882dc66f328a520d2f3d
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-11-02 01:38:39 +0530

    Updated Dtos

--- inventory-supplier-service/src/main/java/com/cg/inventorysupplierservice/dto/DistributorDto.java
+++ inventory-supplier-service/src/main/java/com/cg/inventorysupplierservice/dto/DistributorDto.java
@@ -1,9 +1,13 @@
+/**
+ * @author Gagandeep Singh
+ * @email singh.gagandeep3911@gmail.com
+ * @create date 2020-11-02 01:09:45
+ * @modify date 2020-11-02 01:09:45
+ * @desc Distributor Dto
+ */
 package com.cg.inventorysupplierservice.dto;
 
-import javax.persistence.Column;
-import javax.validation.constraints.Max;
-import javax.validation.constraints.Min;
-import javax.validation.constraints.NotNull;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
 import lombok.AllArgsConstructor;
@@ -17,15 +21,16 @@ import lombok.NoArgsConstructor;
 @Data
 public class DistributorDto {
 
-	@Min(100000)
-	@Max(999999)
 	private Long distributorId;
 
-	@Size(min = 5, max = 40, message = "Length should be between 5 to 40 characters")
-	@NotNull(message = "This field cannot be null")
-	private String materialName;
+	@NotBlank
+	@Size(min = 5, max = 50)
+	private String name;
+	@NotBlank
+	@Size(min = 5, max = 50)
+	private String location;
+	@NotBlank
+	@Size(min = 10, max = 10, message = "Length of phone number must be 10")
+	private String phoneNo;
 
-	@Size(min = 5, max = 100, message = "Length should be between 5 to 100 characters")
-	@NotNull(message = "This field cannot be null")
-	private String description;
 }
