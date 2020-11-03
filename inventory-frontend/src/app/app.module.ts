a7c9d61e92178eba048d94107c291f9b4b836821
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-11-03 09:01:12 +0530

    Created navigation component

--- inventory-frontend/src/app/app.module.ts
+++ inventory-frontend/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './shared/footer/footer.component';
+import { NavigationComponent } from './shared/navigation/navigation.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FooterComponent
+    FooterComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule,
