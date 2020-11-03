8660c184c82976c910ea7558b07ebaa805144b13
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-11-03 15:29:48 +0530

    Created distrituor components

---
+++ inventory-frontend/src/app/distributor/view-distributor/view-distributor.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-view-distributor',
+  templateUrl: './view-distributor.component.html',
+  styleUrls: ['./view-distributor.component.css']
+})
+export class ViewDistributorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
