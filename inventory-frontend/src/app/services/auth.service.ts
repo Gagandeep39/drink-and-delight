98def2938bb0b9cccb7e3182c4facad1d9019f1a
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-11-04 01:15:12 +0530

    Implemented login in auth service

--- inventory-frontend/src/app/services/auth.service.ts
+++ inventory-frontend/src/app/services/auth.service.ts
@@ -1,3 +1,11 @@
+/**
+ * @author Gagandeep Singh
+ * @email singh.gagandeep3911@gmail.com
+ * @create date 2020-11-04 01:14:46
+ * @modify date 2020-11-04 01:14:46
+ * @desc Auth Service
+ */
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 
@@ -5,7 +13,11 @@ import { environment } from 'src/environments/environment';
   providedIn: 'root',
 })
 export class AuthService {
-  applicationUrl = environment.applicationUrl;
+  authServiceUrl = `http://${environment.applicationUrl}/${environment.authService}/`;
 
-  constructor() {}
+  constructor(private http: HttpClient) {}
+
+  login (formData) {
+    return this.http.post(this.authServiceUrl + 'auth/login', formData);
+  }
 }
