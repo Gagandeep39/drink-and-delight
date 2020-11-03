bf4e66e4c6bea76db87908e316382c51a239fe48
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-11-04 01:27:25 +0530

    Login response now gives role and username

--- inventory-auth-service/src/main/java/com/cg/inventoryauthservice/dto/LoginResponse.java
+++ inventory-auth-service/src/main/java/com/cg/inventoryauthservice/dto/LoginResponse.java
@@ -8,15 +8,19 @@
 package com.cg.inventoryauthservice.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
 @Data
 @NoArgsConstructor
+@Builder
 public class LoginResponse {
 
   private Long userId;
   private String token;
-  
+  private String role;
+  private String username;
+
 }
