1cf1e6a83d9cade564e7a83076e1894a2a3afd90
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-11-04 01:40:53 +0530

    Added more functions to auth service

--- inventory-frontend/src/app/services/auth.service.ts
+++ inventory-frontend/src/app/services/auth.service.ts
@@ -8,6 +8,9 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
+import { User } from '../models/user.model';
+import { tap } from 'rxjs/operators';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root',
@@ -15,9 +18,25 @@ import { environment } from 'src/environments/environment';
 export class AuthService {
   authServiceUrl = `http://${environment.applicationUrl}/${environment.authService}/`;
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private router: Router) {}
 
-  login (formData) {
-    return this.http.post(this.authServiceUrl + 'auth/login', formData);
+  login(formData) {
+    return this.http
+      .post(this.authServiceUrl + 'auth/login', formData)
+      .pipe(tap((user: User) => this.saveToSessionStorage(user)));
+  }
+
+  saveToSessionStorage(user: User) {
+    sessionStorage.setItem('user', JSON.stringify(user));
+  }
+
+  fetchFromSessionStorage() {
+    return JSON.parse(sessionStorage.getItem('user'));
+  }
+
+  logout(): void {
+    sessionStorage.clear();
+    localStorage.clear();
+    this.router.navigate(['/login']);
   }
 }
