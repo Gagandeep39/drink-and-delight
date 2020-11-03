f035173c574fa39e5e94a34cd4c314ebfc60bbb4
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-11-03 14:58:02 +0530

    Added all modules in root module

--- inventory-frontend/src/app/app.module.ts
+++ inventory-frontend/src/app/app.module.ts
@@ -5,12 +5,38 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { NavigationComponent } from './shared/navigation/navigation.component';
+import { ViewProductsComponent } from './products/view-products/view-products.component';
+import { AddProductComponent } from './products/add-product/add-product.component';
+import { AddRawMaterialComponent } from './raw-materials/add-raw-material/add-raw-material.component';
+import { ViewRawMaterialComponent } from './raw-materials/view-raw-material/view-raw-material.component';
+import { ViewRawMaterialOrdersComponent } from './raw-materials/view-raw-material-orders/view-raw-material-orders.component';
+import { AddRawMaterialOrderComponent } from './raw-materials/add-raw-material-order/add-raw-material-order.component';
+import { AddProductOrderComponent } from './products/add-product-order/add-product-order.component';
+import { ViewProductOrdersComponent } from './products/view-product-orders/view-product-orders.component';
+import { AddUserComponent } from './users/add-user/add-user.component';
+import { ViewUsersComponent } from './users/view-users/view-users.component';
+import { ViewProfileComponent } from './profile/view-profile/view-profile.component';
+import { LoginComponent } from './auth/login/login.component';
+import { ChangePasswordComponent } from './auth/change-password/change-password.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FooterComponent,
-    NavigationComponent
+    NavigationComponent,
+    ViewProductsComponent,
+    AddProductComponent,
+    AddRawMaterialComponent,
+    ViewRawMaterialComponent,
+    ViewRawMaterialOrdersComponent,
+    AddRawMaterialOrderComponent,
+    AddProductOrderComponent,
+    ViewProductOrdersComponent,
+    AddUserComponent,
+    ViewUsersComponent,
+    ViewProfileComponent,
+    LoginComponent,
+    ChangePasswordComponent
   ],
   imports: [
     BrowserModule,
