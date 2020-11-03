606ae80e771c6c130ae506d95ceab1ea71a9e208
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-11-03 08:48:59 +0530

    Removed dummy data

--- inventory-frontend/src/app/app.component.spec.ts
+++ inventory-frontend/src/app/app.component.spec.ts
@@ -13,23 +13,4 @@ describe('AppComponent', () => {
       ],
     }).compileComponents();
   });
-
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
-
-  it(`should have as title 'inventory-frontend'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('inventory-frontend');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('inventory-frontend app is running!');
-  });
 });
