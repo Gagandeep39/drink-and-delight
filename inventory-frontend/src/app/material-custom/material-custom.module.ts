a7e76e09c8bd37c132c803caf519af0e5fd94909
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-11-05 02:34:20 +0530

    Added mat input module

--- inventory-frontend/src/app/material-custom/material-custom.module.ts
+++ inventory-frontend/src/app/material-custom/material-custom.module.ts
@@ -13,6 +13,7 @@ import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatTableModule } from '@angular/material/table';
 import { MatSortModule } from '@angular/material/sort';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 import { MatCardModule } from '@angular/material/card';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
@@ -28,6 +29,7 @@ import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
     MatCardModule,
     MatFormFieldModule,
     MatProgressSpinnerModule,
+    MatInputModule,
   ],
   exports: [
     MatDialogModule,
@@ -38,6 +40,7 @@ import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
     MatCardModule,
     MatFormFieldModule,
     MatProgressSpinnerModule,
+    MatInputModule,
   ],
 })
 export class MaterialCustomModule {}
