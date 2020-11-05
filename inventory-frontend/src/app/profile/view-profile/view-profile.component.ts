42abaf9fcc0a47327b46e9e0c1c4b0c4e19bfafd
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-11-06 02:50:47 +0530

    Implemented View profile

--- inventory-frontend/src/app/profile/view-profile/view-profile.component.ts
+++ inventory-frontend/src/app/profile/view-profile/view-profile.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { UserDetails } from 'src/app/models/user-details.model';
+import { AuthService } from 'src/app/services/auth.service';
+import { LoadingService } from 'src/app/services/loading.service';
+import { ManageUserService } from 'src/app/services/manage-user.service';
 
 @Component({
   selector: 'app-view-profile',
   templateUrl: './view-profile.component.html',
-  styleUrls: ['./view-profile.component.css']
+  styleUrls: ['./view-profile.component.css'],
 })
 export class ViewProfileComponent implements OnInit {
+  user: UserDetails;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    public loadingService: LoadingService,
+    private manageUser: ManageUserService,
+    private authService: AuthService
+  ) {
+    this.fetchUser();
   }
 
+  fetchUser() {
+    this.manageUser
+      .fetchById(this.authService.fetchFromSessionStorage()?.userId)
+      .subscribe((res: UserDetails) => (this.user = res));
+  }
+
+  ngOnInit(): void {}
 }
