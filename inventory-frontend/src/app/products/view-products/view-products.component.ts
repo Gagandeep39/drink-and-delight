c1c991178db1738a9704713df983e4bae70f6c5c
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-11-08 23:55:14 +0530

    Implemtend View Product

--- inventory-frontend/src/app/products/view-products/view-products.component.ts
+++ inventory-frontend/src/app/products/view-products/view-products.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Product } from 'src/app/models/product.model';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-view-products',
   templateUrl: './view-products.component.html',
-  styleUrls: ['./view-products.component.css']
+  styleUrls: ['./view-products.component.css'],
 })
 export class ViewProductsComponent implements OnInit {
+  products: Product[] = [];
 
-  constructor() { }
+  constructor(private productService: ProductService, private router: Router) {}
 
   ngOnInit(): void {
+    this.productService.fetchAllProducts().subscribe((data: Product[]) => {
+      this.products = data;
+    });
   }
 
+  navigate(): void {
+    this.router.navigate(['/dashboard/addproduct']);
+  }
 }
