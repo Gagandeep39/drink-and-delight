380bb276ebf28956f74dc832535a67d18c4dc9c4
Gagandeep Singh <gagandeep@Manmohans-MacBook.local>
2020-11-08 12:23:38 +0530

    Removed unused imoports

--- inventory-gateway-server/src/main/java/com/cg/inventorygatewayserver/InventoryGatewayServerApplication.java
+++ inventory-gateway-server/src/main/java/com/cg/inventorygatewayserver/InventoryGatewayServerApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableZuulProxy
--- inventory-gateway-server/src/main/java/com/cg/inventorygatewayserver/config/ZuulConfig.java
+++ inventory-gateway-server/src/main/java/com/cg/inventorygatewayserver/config/ZuulConfig.java
@@ -7,12 +7,7 @@
  */
 package com.cg.inventorygatewayserver.config;
 
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 public class ZuulConfig {
