2da114f03ed35f3f363d7682110f4c919ac1b3e7
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-11-08 22:57:57 +0530

    Created View Supplier

--- inventory-frontend/src/app/supplier/view-supplier/view-supplier.component.ts
+++ inventory-frontend/src/app/supplier/view-supplier/view-supplier.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Supplier } from 'src/app/models/supplier.model';
+import { SupplierService } from 'src/app/services/supplier.service';
 
 @Component({
   selector: 'app-view-supplier',
   templateUrl: './view-supplier.component.html',
-  styleUrls: ['./view-supplier.component.css']
+  styleUrls: ['./view-supplier.component.css'],
 })
 export class ViewSupplierComponent implements OnInit {
-
-  constructor() { }
+  supplier: Supplier;
+  suppliers: Supplier[];
+  constructor(
+    private supplierService: SupplierService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
+    this.reload();
+    this.supplierService
+      .fetchAllSuppliers()
+      .subscribe((response: Supplier[]) => {
+        this.suppliers = response;
+      });
+  }
+  reload() {
+    this.router.navigate(['/dashboard/suppliers']);
   }
 
+  updateSupplier(id: number) {
+    this.router.navigate(['/dashboard/updatesupplier', id]);
+  }
 }
