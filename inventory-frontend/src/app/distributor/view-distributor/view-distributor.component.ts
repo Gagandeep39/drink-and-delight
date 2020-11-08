d6e8a2c8eb1e60c0a9cda87f28733a88cba6cf60
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-11-08 22:40:58 +0530

    Created View distributor

--- inventory-frontend/src/app/distributor/view-distributor/view-distributor.component.ts
+++ inventory-frontend/src/app/distributor/view-distributor/view-distributor.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Distributor } from 'src/app/models/distributor.model';
+import { DistributorService } from 'src/app/services/distributor.service';
 
 @Component({
   selector: 'app-view-distributor',
   templateUrl: './view-distributor.component.html',
-  styleUrls: ['./view-distributor.component.css']
+  styleUrls: ['./view-distributor.component.css'],
 })
 export class ViewDistributorComponent implements OnInit {
-
-  constructor() { }
+  distributor: Distributor;
+  distributors: Distributor[];
+  constructor(
+    private distributorService: DistributorService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
+    this.distributorService
+      .fetchAllDistributors()
+      .subscribe((response: Distributor[]) => {
+        this.distributors = response;
+      });
   }
 
+  updateDistributor(id: number) {
+    console.log(id);
+    
+    this.router.navigate(['/dashboard/updatedistributor', id]);
+  }
 }
