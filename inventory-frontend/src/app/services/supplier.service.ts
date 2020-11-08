b76d3db9ec8abe9d96bb7949dc7297d910afaf26
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-11-08 19:22:04 +0530

    Implemented Supplier Servie

--- inventory-frontend/src/app/services/supplier.service.ts
+++ inventory-frontend/src/app/services/supplier.service.ts
@@ -1,11 +1,27 @@
+/**
+ * @author Gagandeep Singh
+ * @email singh.gagandeep3911@gmail.com
+ * @create date 2020-11-08 18:35:38
+ * @modify date 2020-11-08 18:35:38
+ * @desc Manage Supplier related operation
+ */
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class SupplierService {
-  applicationUrl = environment.applicationUrl;
+  serviceUrl =
+    environment.protocol +
+    environment.applicationUrl +
+    '/' +
+    environment.supplierService;
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  fetchAllSuppliers() {
+    return this.http.get(this.serviceUrl + '/supplier');
+  }
 }
