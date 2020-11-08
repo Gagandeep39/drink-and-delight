25bce5e6160e49e8b4b19cf6603d3609ca51a196
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-11-08 22:57:37 +0530

    Created Add supplier

--- inventory-frontend/src/app/supplier/add-supplier/add-supplier.component.ts
+++ inventory-frontend/src/app/supplier/add-supplier/add-supplier.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Supplier } from 'src/app/models/supplier.model';
+import { SupplierService } from 'src/app/services/supplier.service';
 
 @Component({
   selector: 'app-add-supplier',
   templateUrl: './add-supplier.component.html',
-  styleUrls: ['./add-supplier.component.css']
+  styleUrls: ['./add-supplier.component.css'],
 })
 export class AddSupplierComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  supplier: Supplier;
+  constructor(private service: SupplierService, private router: Router) {
+    this.supplier = new Supplier();
   }
 
+  ngOnInit(): void {}
+
+  addSupplier() {
+    this.service.addSupplier(this.supplier).subscribe((res) => {
+      this.router.navigate(['/dashboard/suppliers']);
+    });
+  }
 }
