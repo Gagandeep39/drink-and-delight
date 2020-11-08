8434c3f68f918ca758425343170a15b75ff095e4
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-11-08 23:55:06 +0530

    Implemented Add product

--- inventory-frontend/src/app/products/add-product/add-product.component.ts
+++ inventory-frontend/src/app/products/add-product/add-product.component.ts
@@ -1,15 +1,41 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Product } from 'src/app/models/product.model';
+import { Warehouse } from 'src/app/models/warehouse.model';
+import { ProductService } from 'src/app/services/product.service';
+import { WarehouseService } from 'src/app/services/warehouse.service';
 
 @Component({
   selector: 'app-add-product',
   templateUrl: './add-product.component.html',
-  styleUrls: ['./add-product.component.css']
+  styleUrls: ['./add-product.component.css'],
 })
 export class AddProductComponent implements OnInit {
+  product: Product;
+  warehouses: Warehouse[] = [];
+  warehouseId;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private service: ProductService,
+    private warehouseService: WarehouseService,
+    private router: Router
+  ) {
+    this.product = new Product();
+    this.warehouseService.fetchAllWarehouse().subscribe((data: Warehouse[]) => {
+      this.warehouses = data;
+    });
   }
 
+  ngOnInit(): void {
+    this.product = new Product();
+  }
+
+  saveProduct(formData) {
+    let warhouse = new Warehouse();
+    warhouse.warehouseId = +formData.warehouseId;
+    this.product.warehouse = warhouse;
+    this.service.addProduct(this.product).subscribe((res) => {
+      this.router.navigate(['/dashboard/products']);
+    });
+  }
 }
